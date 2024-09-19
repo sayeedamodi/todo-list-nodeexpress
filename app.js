4c9bc3d5bef86f1b90f531111cb13c4abbe02a50
SHADOWArab <87262605+SHADOWArab@users.noreply.github.com>
2024-09-19 21:18:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app= express();
+var  items = [];
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req,res) {
+  var today = new Date();
+  var options ={
+    weekday:"long",
+    day:"numeric",
+    month : "long",
+    year :"numeric"
+  } ;
+
+  var day = today.toLocaleDateString("en-US",options);
+  res.render("list",{kindOfday:day , newListItems:items});
+
+});
+
+app.post("/", function(req,res){
+   var item = req.body.newItem ; 
+    items.push(item);
+    res.redirect("/");
+})
+app.listen(3000, function (){
+    console.log("server is running on port 3000");
+});
