ce7ac7c5f12f08923ec2f8d554d37008f7a66c22
SHADOWArab <87262605+SHADOWArab@users.noreply.github.com>
2024-09-21 14:23:16 +0530

    added Procfile  and custom port

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.post("/", function(req,res){
     items.push(item);
     res.redirect("/");
 })
-app.listen(3000, function (){
+app.listen(process.env.PORT || 3000, function (){
     console.log("server is running on port 3000");
 });
